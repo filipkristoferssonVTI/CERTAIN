4d19efb741fbc020782ab8a3d0de94fa64909885
filipkristoferssonVTI <filip.kristofersson@VTI.se>
2024-11-29 09:49:24 +0100

    Create structure_learning.py

---
+++ structure_learning.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pandas as pd
+from pgmpy.estimators import HillClimbSearch
+
+from bayesian_network import filter_data
+from utils.model import plot_model
+
+
+def main():
+    data_folder = Path('data')
+    df = pd.read_csv(data_folder / 'Kopia av Daedalos export - Insatta resurser 2201 2411.csv', sep=';')
+
+    df = filter_data(df)
+
+    est = HillClimbSearch(df)
+    dag = est.estimate(scoring_method='k2score')
+
+    plot_model(dag)
+
+
+if __name__ == '__main__':
+    main()
