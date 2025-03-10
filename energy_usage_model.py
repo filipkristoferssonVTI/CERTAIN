a0e646764eca7242bef191d322ac84a1c9ebbd22
GabriellaGrenander <33859046+GabriellaGrenander@users.noreply.github.com>
2025-03-10 15:28:48 +0100

    Update energy_usage_model.py

--- energy_usage_model.py
+++ energy_usage_model.py
@@ -11,6 +11,29 @@ from gis_analysis import create_population_grid
 
 battery_capacity = 540 #kWh
 
+def calculate_efficiency(v):
+    # https://publications.lib.chalmers.se/records/fulltext/133658.pdf
+    # Formula and rolling resitance value from page 26
+    # Other values from page 31, Volvo FH Summer
+    #
+    # Literature says the FH electric takes 1.1 kWh/km, numbers below gives 1.04 kWh/km.
+    # Can probably be calibrated
+    #
+    # Assumes v in km/h, conversion to m/s in function
+
+    v = v/3.6 # m/s, vehicle velocity
+
+    rho = 1.228 # km/m^3, air density
+    Cd = 0.59 # const, drag coefficient
+    A = 9.7 # m^2, frontal area
+    m = 40000 # kg, mass of vehicle
+    g = 9.82 # m/s^2, gravitational acceleration (Sweden)
+    fr = 0.0052 # rolling resistance 
+
+    conv_factor = 2.77/10000 # Converting from N to kWh/km
+    
+    return (1/2*rho*Cd*A*v*v + m*g*fr)*conv_factor
+
 
 def calculate_kWh_travel(km):
     # This energy efficiency is based on a 40 ton volvo electric truck, with an average speed of 80 km/h
@@ -71,21 +94,33 @@ def main():
 
     data_folder = Path('data')
 
+    # Process fire station data into gdf
     fire_stations = process_fire_stations(data_folder)
 
+    # Make grid gdf that corresponds with Filips grid
     grid = create_population_grid(data_folder)
     grid['rut_id'].astype(str)
 
+    # Process OD-matrix from GIS and add to grid gdf
     od_matrix = process_od_matrix(data_folder)
-
     grid = pd.merge(grid, od_matrix, on='rut_id', how='inner')
     grid.to_file(data_folder / 'output' / 'grid_od_matrix.gpkg', driver='GPKG')
 
+    # Plot firestations on a map
     plot_fire_stations_map(fire_stations)
 
+    # Plot choropleth map of distance to grid cell from firestation
     plot_distance_from_firestation_map(grid, fire_stations, fire_station_plot)
 
+    velocity_list = list(range(5,150,5))
+    efficiency_list = [calculate_efficiency(v) for v in velocity_list]
+
+    fig = px.scatter(x = velocity_list, y = efficiency_list)
+    fig.show()
+
     grid['energy_usage'] = calculate_kWh_travel(grid['dist_km'])
+
+    
 
 
 
